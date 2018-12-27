8787449e43aca498febb09496923a644eacd3fe6
Chris Blakley <greatblakes@gmail.com>
2018-12-27 09:05:34 -0500

    Design metabox updates

--- nebula-companion.php
+++ nebula-companion.php
@@ -3,7 +3,7 @@
 Plugin Name: Nebula Companion
 Plugin URI: https://gearside.com/nebula/
 Description: Advanced features for use with the Nebula theme.
-Version: 6.7.21.6561
+Version: 6.7.27.3731
 Author: Chris Blakley
 Author URI: https://gearside.com/nebula
 */
